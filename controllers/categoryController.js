82ce6e83a42811bf2edd68e57617e482f416ea93
Mirzobakhtiyor Homidov <khamidov.mirzo@gmail.com>
2023-02-28 21:15:31 +0800

    implement form handlers in categoryController.js

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,11 +1,11 @@
 const Category = require("../models/category");
-const Item = require("../models/item")
+const Item = require("../models/item");
 
 // async library to work with asynchronous operations
 const async = require("async");
 
 // express-validator module to sanitize forms
-const {body, validationResult} = require("express-validator")
+const { body, validationResult } = require("express-validator");
 
 // Display list of all Categories
 exports.category_list = (req, res, next) => {
@@ -29,11 +29,13 @@ exports.category_list = (req, res, next) => {
 exports.category_detail = (req, res, next) => {
   async.parallel(
     {
-      category(callback) { // get given category's info from database
+      category(callback) {
+        // get given category's info from database
         Category.findById(req.params.id).exec(callback);
       },
 
-      category_items(callback) { // get items that contain the given category in their category array
+      category_items(callback) {
+        // get items that contain the given category in their category array
         Item.find({ category: req.params.id }).exec(callback);
       },
     },
@@ -47,7 +49,7 @@ exports.category_detail = (req, res, next) => {
         err.status = 404;
         return next(err);
       }
-      console.log(results.category_items)
+      console.log(results.category_items);
       // Successful, so render
       res.render("pages/category_detail", {
         page_title: "Category Detail",
@@ -57,19 +59,62 @@ exports.category_detail = (req, res, next) => {
       });
     }
   );
-
-
 };
 
 // Display Category create from GET
 exports.category_create_get = (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Category create GET");
+  res.render("pages/category_form", {
+    title: "Category form",
+    page_title: "Category form",
+  });
 };
 
 // Handle Category create on POST.
-exports.category_create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Category create POST");
-};
+exports.category_create_post = [
+  // Validate and sanitize the name field.
+  body("name", "Category name required").trim().isLength({ min: 1 }).escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+    const category = new Category({ name: req.body.name });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("genre_form", {
+        title: "Create Category",
+        page_title: "Create Category",
+        category: category,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      // Check if Category with same name already exists.
+      Category.findOne({ name: req.body.name }).exec((err, found_category) => {
+        if (err) {
+          return next(err);
+        }
+
+        if (found_category) {
+          // Category exists, redirect to its detail page.
+          res.redirect(found_category.url);
+        } else {
+          category.save((err) => {
+            if (err) {
+              return next(err);
+            }
+            // Genre saved. Redirect to genre detail page.
+            res.redirect(category.url);
+          });
+        }
+      });
+    }
+  },
+];
 
 // Display Category delete form on GET
 exports.category_delete_get = (req, res) => {
